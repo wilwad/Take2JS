99bc397bd6f78e93172b2d8fa4d210102b166b78
William Sengdara <william.sengdara@gmail.com>
2022-03-09 13:37:44 +0200

    speechrecognition

--- js/test.js
+++ js/test.js
@@ -13,7 +13,52 @@ class test {
     }
 }
 
-let x = new test();
-console.log(x.vals)
-x.vals = [2,3]
-console.log(x.vals)
+//let x = new test();
+//console.log(x.vals)
+//x.vals = [2,3]
+//console.log(x.vals)
+var phrases = 'orange|yellow|blue|green|red|black|white|pink|gold|magenta|close|full screen'.split('|')
+var recog = new ChromeSpeechRecognition(phrases)
+recog.onresult = function(phrase, confidence){
+    phrase = phrase.toLowerCase()
+    console.log('I heard you say', phrase)
+    if (self.phrases.indexOf(phrase) > -1) 
+        if (phrase == 'close') 
+            window.close()
+        else if (phrase == 'full screen')
+            openFullscreen(document.body)
+        else
+            document.body.style.backgroundColor = phrase
+    else
+        console.log('unrecognized phrase', phrase)    
+}
+
+function openFullscreen(elem) {
+  if (elem.requestFullscreen) {
+    elem.requestFullscreen();
+  } else if (elem.webkitRequestFullscreen) { /* Safari */
+    elem.webkitRequestFullscreen();
+  } else if (elem.msRequestFullscreen) { /* IE11 */
+    elem.msRequestFullscreen();
+  }
+}
+
+/* Swipe left/right on Tinder using speechrecognition */
+
+var recog = new ChromeSpeechRecognition(['like','dislike')
+recog.onresult = function(phrase, confidence){
+    phrase = phrase.toLowerCase()
+    console.log('I heard you say', phrase)
+    if (self.phrases.indexOf(phrase) > -1) 
+        switch (phrase){
+            case 'like':
+                document.querySelector('button[data-testid="gamepadLike"]').click()
+                break;
+                
+            case 'dislike':
+                document.querySelector('button[data-testid="gamepadDislike"]').click()
+                break;
+        }
+    else
+        console.log('unrecognized phrase', phrase)    
+}
